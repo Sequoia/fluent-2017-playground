b0b6334e4dbc780f67078d089b0920ca6d2b1cb0
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2017-05-26 16:36:22 -0400

    redis user info stored in session

--- auth-service-redis-mongo/index.js
+++ auth-service-redis-mongo/index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 8080;
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
 const cookieParser = require('cookie-parser');
 const passport = require('./passportLocal');
 
@@ -13,7 +14,13 @@ const app = express();
 app.use(express.static('public'));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:false}));
+// app.use(session({
+//   secret: 'keyboard cat',
+//   saveUninitialized : false,  // create session for non-logged-in users
+//   resave : false              // save back to session store for each request
+// }));
 app.use(session({
+  store: new RedisStore({ url: process.env.REDIS_SESSION_URL }),
   secret: 'keyboard cat',
   saveUninitialized : false,  // create session for non-logged-in users
   resave : false              // save back to session store for each request
@@ -21,16 +28,32 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+function loggedIn(req, res, next){
+  if(!req.user){
+    //not logged in!
+    return res.redirect('/');
+  }
+  else{
+    next();
+  }
+}
+
 // routes
 app.get('/', function sendLoginForm(req, res){
   res.sendFile('./public/login.html', {
     root: __dirname
   });
-})
+});
 
-app.get('/account', function accountJSON(req, res){
-  console.log(req.user);
+app.get('/account', loggedIn, function accountJSON(req, res){
   res.json(req.user);
+});
+
+app.get('/add_one', function addOne(req, res){
+  req.session.count = req.session.count ? req.session.count + 1 : 1;
+  res.json({
+    count: req.session.count
+  });
 })
 
 app.post('/login', passport.authenticate('local', {
--- auth-service-redis-mongo/passportLocal.js
+++ auth-service-redis-mongo/passportLocal.js
@@ -1,20 +1,58 @@
+require('dotenv').config();
+
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('./User');
 
+// REDIS serialization
+
+const redis = require('redis');
+const client = redis.createClient({
+  url : process.env.REDIS_SESSION_URL
+});
+
+// END REDIS serialization
+
+//// IN MEMORY serialization
 const users = {};
+
+function serializeJustId(user, done) {
+  users[user.id] = user.toJSON();
+  done(null, user.id);
+}
+
+function deserializeById(id, done) {
+  if(users[id]){
+    done(null, users[id]);
+  }else{
+    done(null, false);
+  }
+}
+/// END IN MEMORY serialization
+
+passport.serializeUser(function serializeToRedis(user, done){
+  client.set(user.id, JSON.stringify(user), function setComplete(err, response){
+    if(err) return done(err);
+    else done(null, user.id)
+  });
+});
+
+passport.deserializeUser(function deserializeFromRedis(id, done){
+  client.get(id, function getComplete(err, value){
+    try{
+      if(value){
+        return done(null, JSON.parse(value)); //user session found
+      }else{
+        return done(null, false); // no user
+      }
+    }catch(e){
+      done(e);
+    }
+  });
+});
 
 const mongooseStrategy = new LocalStrategy(checkMongoLogin);
 passport.use('local', mongooseStrategy);
-
-passport.serializeUser(function serializeJustId(user, done) {
-  users[user.id] = user.toJSON();
-  done(null, user.id);
-});
-
-passport.deserializeUser(function desrializeById(id, done) {
-  done(null, users[id]);
-});
 
 /**
  * 
